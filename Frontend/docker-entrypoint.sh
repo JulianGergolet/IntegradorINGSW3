18b153d5e83eb44005b37475700b54d86eda860d
Julian Gergolet <juligergolet@gmail.com>
2023-12-11 18:43:48 -0300

    chan

--- Frontend/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Reemplazar la cadena ${PORT} en nginx.conf con el valor proporcionado por Heroku
-sed -i 's/${PORT}/'"$PORT"'/g' /etc/nginx/nginx.conf
-
-# Iniciar Nginx
-nginx -g 'daemon off;'
