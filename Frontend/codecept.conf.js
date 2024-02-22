7750bde934eb5863c6a4b5ebed1b6e26cf8bebc6
Julian Gergolet <juligergolet@gmail.com>
2024-02-21 23:59:29 -0300

    mas

--- Frontend/codecept.conf.js
+++ Frontend/codecept.conf.js
@@ -7,9 +7,6 @@ exports.config = {
       browser: 'chromium',
       url: 'https://ingsw3frontend-e0243388a7b1.herokuapp.com/',
       show: true,
-      chromium: {
-        executablePath: 'C:\\Users\\Usuario\\AppData\\Local\\Chromium\\Application\\chrome.exe'
-      }
     }
   },
   include: {
