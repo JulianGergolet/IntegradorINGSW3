8d93f3fb09fa6df98a75d5691b64330ab128bc17
Julian Gergolet <juligergolet@gmail.com>
2024-07-05 14:09:24 -0300

    mas

--- Backend/Tests.js
+++ Backend/Tests.js
@@ -8,7 +8,7 @@ describe('Backend API Tests', () => {
         const response = await fetch('http://localhost:3000/');
         const data = await response.text();
 
-        assert.equal(data, 'Hello Worldd');
+        assert.equal(data, 'Hello World');
     });
 
     // Prueba para verificar la inserción de una transacción en la base de datos
